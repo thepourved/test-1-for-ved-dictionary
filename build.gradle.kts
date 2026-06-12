78b7245f537d34d2e95ae58ea5c7f00d65e11cf9
thepourved <thepouriavedad@gmail.com>
2026-06-12 15:28:31 +0330

    Fix: Update build.gradle.kts with proper plugin configuration and buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 task("clean", Delete::class) {
